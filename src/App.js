d13d2651778e6b00098e54ef8931252eb7b6c3bc
Andrea Mestre <amestre89@gmail.com>
2021-02-08 14:19:19 +0100

    Exercice 1 & 2 done

--- src/App.js
+++ src/App.js
@@ -7,17 +7,49 @@ export default function App() {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
 
-  const handleChange = e => {
+  const handleChange = ({ target }) => {
     // handle key presses
+    const value = target.value;
+    setLocation(value);
   };
 
   const handleSubmit = e => {
     // handle form submit
+    e.preventDefault();
+    getWeather(location);
+    setLoading(true);
   };
 
-  const getWeather = () => {
+  const getWeather = location => {
+    console.log(location);
     // call Open Weather API
+    const myApiKey = "e5fe5219a05002ab0dc746280be28be0";
+    fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        location +
+        "&appid=" +
+        myApiKey
+    )
+      .then(res => res.json())
+      .then(json => setWeather(json))
+      .catch(error => setError(error));
   };
 
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      Hello World!
+      <div>
+        <form>
+          <label>
+            Location:
+            <input type="text" onChange={handleChange}></input>
+            <button onClick={handleSubmit}>Submit</button>
+          </label>
+        </form>
+        <div>The weather in {location} is:</div>
+      </div>
+    </div>
+  );
 }
+
+//"api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + myApiKey
